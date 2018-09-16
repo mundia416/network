aa457d1b4aa9a7cbfb0a69a37348c2e9ecbc2098
Mundia <fxmundia416@gmail.com>
2018-09-16 18:19:50 +0200

    bug fix

---
+++ retrofitlib/src/main/java/com/nosetrap/retrofitlib/ConnectionErrorManager.kt
@@ -0,0 +1,26 @@
+package com.nosetrap.retrofitlib
+
+import java.net.ConnectException
+import java.net.SocketTimeoutException
+import java.net.UnknownHostException
+
+/**
+ * class that deals with the different types of connection errors
+ */
+class ConnectionErrorManager(private val callback: ConnectionErrorCallback) {
+
+    /**
+     * process an Exception/throwable and execute the right callback methods
+     * this method is used in the onError method in retrofit2 callback
+     */
+    fun handleError(error: Throwable) {
+        if (error is ConnectException ||
+                error is SocketTimeoutException ||
+                error is UnknownHostException) {
+            callback.onConnectionError()
+        } else {
+            callback.onRandomError()
+        }
+        //todo handle all other exceptions that could possibly be thrown
+    }
+}
