2f226b73752882feae0bf36eeff86f610e7cc35a
Mundia <fxmundia416@gmail.com>
2018-10-25 09:15:56 +0200

    added utility methods

--- networklib/src/main/java/com/nosetrap/networklib/Connection.kt
+++ networklib/src/main/java/com/nosetrap/networklib/Connection.kt
@@ -11,11 +11,15 @@ import java.net.InetAddress
 import java.util.regex.Pattern
 import android.telephony.TelephonyManager
 import android.net.NetworkInfo
+import android.content.Intent
+import androidx.core.content.ContextCompat.startActivity
 
 
 
 
-class Connection(context: Context) {
+
+
+class Connection(private val context: Context) {
 
     companion object {
 
@@ -43,8 +47,7 @@ class Connection(context: Context) {
 
 
     private val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-
-
+    private val telephonyManager = context.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
 
     /**
      * checks whether there is a connection to the internet, this can be of any type as long as
@@ -61,5 +64,40 @@ class Connection(context: Context) {
             }
         }
         return false
+    }
+
+    /**
+     * checks if the internet connection is a fast internet connection
+     */
+    fun isFastNetwork(): Boolean {
+
+
+        return when (telephonyManager.networkType) {
+            TelephonyManager.NETWORK_TYPE_EVDO_0, TelephonyManager.NETWORK_TYPE_EVDO_A,
+            TelephonyManager.NETWORK_TYPE_HSDPA, TelephonyManager.NETWORK_TYPE_HSPA,
+            TelephonyManager.NETWORK_TYPE_HSUPA, TelephonyManager.NETWORK_TYPE_UMTS,
+            TelephonyManager.NETWORK_TYPE_EHRPD, TelephonyManager.NETWORK_TYPE_EVDO_B,
+            TelephonyManager.NETWORK_TYPE_HSPAP, TelephonyManager.NETWORK_TYPE_LTE -> {
+                true
+            }
+            TelephonyManager.NETWORK_TYPE_1xRTT, TelephonyManager.NETWORK_TYPE_CDMA,
+            TelephonyManager.NETWORK_TYPE_EDGE, TelephonyManager.NETWORK_TYPE_GPRS,
+            TelephonyManager.NETWORK_TYPE_IDEN, TelephonyManager.NETWORK_TYPE_UNKNOWN -> {
+                false
+            }
+            else -> {
+                false
+            }
+        }
+    }
+
+    /**
+     * Open the settings of wireless.
+     */
+    fun openWirelessSettings() {
+        context.startActivity(
+                Intent(android.provider.Settings.ACTION_WIRELESS_SETTINGS)
+                        .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        )
     }
 }
